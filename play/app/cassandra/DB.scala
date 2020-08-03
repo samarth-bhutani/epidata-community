581c947377ce6166a2ef27507e8bf788176ace4d
Samarth Bhutani <samarth.bhutani@berkeley.edu>
2020-08-03 01:35:00 -0700

    update insert w/o test

--- play/app/cassandra/DB.scala
+++ play/app/cassandra/DB.scala
@@ -12,71 +12,165 @@ import com.chrisomeara.pillar.ReplicationOptions
 import com.datastax.driver.core._
 import java.io.File
 import java.io.PrintStream
+
+import java.sql.{Connection, CallableStatement, DriverManager, PreparedStatement, ResultSet, SQLException, Statement}
 import java.util.Date
+
 import org.joda.time.Instant
-import play.api.Logger
-import play.api.Play
-import util.EpidataMetrics
 
 /**
- * Singleton object for managing the server's connection to a Cassandra
+ * Singleton object for managing the server's connection to a Cassandra or SQLite
  * database and executing queries.
  */
 object DB {
-  private var connection: Option[Connection] = None
+  private var connection: Option[ConnectionDB] = None
 
   /**
    * Connect to cassandra. On connect, the keyspace is created and migrated if
    * necessary.
    */
   def connect(nodeNames: String, keyspace: String, username: String, password: String) = {
-    connection = Some(new Connection(nodeNames, keyspace, username, password))
+    connection = Some(new ConnectionCas(nodeNames, keyspace, username, password))
   }
+  /**
+   * Connect to SQlite. 
+   */
+  def connect(url: String) = {
+    connection = Some(new ConnectionSql(url))
+  }
+
 
   /** Generate a prepared statement. */
   def prepare(statementSpec: String) = connection.get.prepare(statementSpec)
 
   /** Execute a previously prepared statement. */
-  def execute(statement: Statement): ResultSet = {
-    val rs = connection.get.execute(statement)
-    rs
-  }
+  def execute(statement: Statement): ResultSet = connection.get.execute(statement)
 
-  /** Execute a previously prepared statement. */
-  def batchExecute(statements: List[Statement]): ResultSet = {
-    val t0 = EpidataMetrics.getCurrentTime
-    val batch = new BatchStatement()
-    statements.foreach(s => batch.add(s))
-    // execute the batch
-    val rs = connection.get.execute(batch)
-    EpidataMetrics.increment("DB.batchExecute", t0)
-    rs
-  }
+  /** Execute a previously prepared statement which returns and empty ResultSet */
+  def executeUpdate(statement: Statement): ResultSet = connection.get.executeUpdate(statement)
 
-  /** Execute a CQL statement by binding ordered attributes. */
-  def cql(statement: String, args: AnyRef*): ResultSet = {
-    connection.get.execute(new SimpleStatement(statement, args: _*))
-  }
+  /** ResultSet to Json Object. */
+//  def resultToJson(rs : ResultSet) = {
+//    // Get the next page info
+//    val nextPage = rs.getExecutionInfo().getPagingState()
+//    val nextBatch = if (nextPage == null) "" else nextPage.toString
+//
+//    // only return the available ones by not fetching.
+//
+//    val rows = 1.to(rs.getFetchSize()).map(_ => rs.one())
+//    val records = new JLinkedList[JLinkedHashMap[String, Object]]()
+//
+//    rows
+//      .map(Model.rowToJLinkedHashMap(_, tableName, modelName))
+//      .foreach(m => records.add(m))
+//
+//    // Return the json object
+//    JsonHelpers.toJson(records, nextBatch)
+//  }
+
+  /** ResultSet to User. */
+
+   /** Executes a batch of statements individually but reverts back to original state if error */
+  def batchExecute(statements: List[Statement]): ResultSet = connection.get.batchExecute(statements)
+
+    /** Binds the values in args to the statement. */
+  def binds(statement: Statement, args: Any*): Statement = connection.get.binds(statement, args)
+    
+  /** Execute a SQL statement by binding ordered attributes. */
+  def cql(statement: String, args: Any*): ResultSet = connection.get.cql(statement, args)
 
   /** Execute a CQL statement by binding named attributes. */
+  def cql(statement: String, args: Map[String, Any]): ResultSet = connection.get.cql(statement, args)
+    
+  /** Closes a Cassandra connection. */
+  def close = connection.get.close 
+
+  /** Returns the ongoing session. */
+  def session = connection.get.session
+
+}
+
+
+private abstract class ConnectionDB {
+
+  val session: Connection
+  def prepare(stm: String) : Statement
+  def execute(stm: Statement) : ResultSet
+  def executeUpdate(stm: Statement) : ResultSet
+  def batchExecute(statements: List[Statement]): ResultSet
+  def binds(statement: Statement, args: Seq[Any]): Statement
+  def cql(statement: String, args: Seq[Any]): ResultSet
+  def cql(statement: String, args: Map[String, Any]): ResultSet
+  def close
+
+}
+
+private class ConnectionLite(url: String) extends ConnectionDB {
+
+  override val session = DriverManager.getConnection(url)
+  val filename = "play/app/conf/pillar/migration/epidatatr.txt"
+  val sql1 = Source.fromFile(filename).getLines.mkString
+  session.createStatement().executeUpdate(sql1)
+
+
+  def prepare(statement: String) : PreparedStatement = session.prepareStatement(statement)
+
+  def execute(statement: Statement) = statement.asInstanceOf[PreparedStatement].executeQuery()
+
+  def executeUpdate(statement: Statement): ResultSet = {
+    statement.asInstanceOf[PreparedStatement].executeUpdate()
+    val l:ResultSet = null
+    l
+  }
+
+  def batchExecute(statements: List[String]) : ResultSet = {
+    val t0 = EpidataMetrics.getCurrentTime
+    var statement = session.createStatement()
+    statements.foreach(s => statement.addBatch(s))
+    statement.executeBatch()
+    EpidataMetrics.increment("DB.batchExecute", t0)
+    val l:ResultSet = null
+    l
+  }
+
+  def binds(statement: Statement, args: Seq[Any]): Statement = {
+    val stm = statement.asInstanceOf[PreparedStatement]
+    var i = 1
+    args.foreach{ e =>
+      stm.setObject(i, e)
+      i+=1
+    }
+    stm
+  }
+
+  def cql(statement: String, args: Seq[Any]): ResultSet = {
+    val boundStatement = prepare(statement)
+    var i = 1
+    for( e <- args){
+      boundStatement.setObject(i, e)
+      i += 1
+    }
+    boundStatement.executeQuery()
+  }
+
   def cql(statement: String, args: Map[String, Any]): ResultSet = {
-    val boundStatement = new BoundStatement(connection.get.prepare(statement))
+    val boundStatement = session.prepareCall(statement)
     args.foreach {
       case (key, value: String) => boundStatement.setString(key, value)
       case (key, value: Int) => boundStatement.setInt(key, value)
       case (key, value: Double) => boundStatement.setDouble(key, value)
-      case (key, value: Date) => boundStatement.setDate(key, LocalDate.fromMillisSinceEpoch(value.getTime))
       case _ => throw new IllegalArgumentException("Unexpected args.")
     }
     connection.get.execute(boundStatement)
   }
 
-  def close {
-    connection.get.close
+  def close = {
+    session.close()
   }
 
-  def session = connection.get.session
 }
+
+
 
 private class TerseMigrationReporter(stream: PrintStream) extends Reporter {
   override def initializing(
@@ -105,7 +199,8 @@ private class TerseMigrationReporter(stream: PrintStream) extends Reporter {
   }
 }
 
-private class Connection(nodeNames: String, keyspace: String, username: String, password: String) {
+
+private class ConnectionCas(nodeNames: String, keyspace: String, username: String, password: String) extends ConnectionDB {
 
   val cluster = nodeNames.split(',').foldLeft(Cluster.builder())({ (builder, nodeName) =>
     try {
@@ -137,6 +232,36 @@ private class Connection(nodeNames: String, keyspace: String, username: String, 
   def prepare(statementSpec: String) = session.prepare(statementSpec)
 
   def execute(statement: Statement) = session.execute(statement)
+
+  def executeUpdate(statement: Statement) = session.execute(statement)
+
+  def batchExecute(statements: List[Statement]): ResultSet = {
+    val t0 = EpidataMetrics.getCurrentTime
+    val batch = new BatchStatement()
+    statements.foreach(s => batch.add(s))
+    // execute the batch
+    val rs = this.execute(batch)
+    EpidataMetrics.increment("DB.batchExecute", t0)
+    rs
+  }
+
+  def binds(statement: Statement, args: AnyRef*): Statement = statement.binds(args)
+
+  def cql(statement: String, args: AnyRef*): ResultSet = {
+    this.execute(new SimpleStatement(statement, args: _*))
+  }
+
+  def cql(statement: String, args: Map[String, Any]): ResultSet = {
+    val boundStatement = new BoundStatement(this.prepare(statement))
+    args.foreach {
+      case (key, value: String) => boundStatement.setString(key, value)
+      case (key, value: Int) => boundStatement.setInt(key, value)
+      case (key, value: Double) => boundStatement.setDouble(key, value)
+      case (key, value: Date) => boundStatement.setDate(key, LocalDate.fromMillisSinceEpoch(value.getTime))
+      case _ => throw new IllegalArgumentException("Unexpected args.")
+    }
+    this.execute(boundStatement)
+  }
 
   def close = {
     session.close()
